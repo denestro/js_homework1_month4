9cdce358317e349154694631336a03597e5c20bc
denestro <tailersam21@gmail.com>
2026-05-11 22:39:15 +0600

    json3

--- js/lesson.js
+++ js/lesson.js
@@ -71,3 +71,47 @@ setInterval(() => {
     showTabContent(currentIndex)
 
 }, 5000)
+
+
+const somInput = document.querySelector('#som')
+const usdInput = document.querySelector('#usd')
+const eurInput = document.querySelector('#eur')
+
+const converter = (element, targetElement1, targetElement2) => {
+    element.oninput = () => {
+
+        const request = new XMLHttpRequest()
+        request.open('GET', '../data/converter.json')
+        request.setRequestHeader('Content-type', 'application/json')
+        request.send()
+
+        request.onload = () => {
+
+            const data = JSON.parse(request.response)
+
+            if (element.id === 'som') {
+                targetElement1.value = (element.value / data.usd).toFixed(2)
+                targetElement2.value = (element.value / data.eur).toFixed(2)
+            }
+
+            if (element.id === 'usd') {
+                targetElement1.value = (element.value * data.usd).toFixed(2)
+                targetElement2.value = ((element.value * data.usd) / data.eur).toFixed(2)
+            }
+
+            if (element.id === 'eur') {
+                targetElement1.value = (element.value * data.eur).toFixed(2)
+                targetElement2.value = ((element.value * data.eur) / data.usd).toFixed(2)
+            }
+
+            if (element.value === '') {
+                targetElement1.value = ''
+                targetElement2.value = ''
+            }
+        }
+    }
+}
+
+converter(somInput, usdInput, eurInput)
+converter(usdInput, somInput, eurInput)
+converter(eurInput, somInput, usdInput)
